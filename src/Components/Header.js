5f0dba4067de44ac6c447dae4f780ed46249852a
Tushar <93274939+Tusharpal353@users.noreply.github.com>
2023-12-27 18:36:55 +0530

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <divv className="Header">
+      <div className="logoContainer">
+        <img
+          className="logo"
+          src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148987940.jpg?w=740&t=st=1698662329~exp=1698662929~hmac=781a955b9b8b178528124e0987986e8929c29174dd334b58fd1db4930c91620d"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us </li>
+          <li>Contact us</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </divv>
+  );
+};
+
+export default Header;
